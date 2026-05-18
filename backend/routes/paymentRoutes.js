8463e750a809112f721d953b5cf1b3013d83c0b3
Prince22 <112156035+princebhandarii@users.noreply.github.com>
2026-05-18 22:44:29 +0530

    Delete backend directory

--- backend/routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const r = express.Router();
-const c = require('../controllers/paymentController');
-const { protect } = require('../middleware/auth');
-
-r.post('/order/:courseId', protect, c.createOrder);
-r.post('/verify',          protect, c.verifyPayment);
-
-module.exports = r;
