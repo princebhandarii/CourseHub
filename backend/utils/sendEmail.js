eb28a863f0460407da584beb08535abc1599b49b
Prince22 <112156035+princebhandarii@users.noreply.github.com>
2026-05-20 22:09:51 +0530

    Switch email sending from nodemailer to Resend

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,27 +1,29 @@
-const nodemailer = require('nodemailer');
+const { Resend } = require('resend');
 
-const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  secure: false,
-
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ to, subject, text, html }) => {
 
-  const mailOptions = {
-    from: `"CourseHub" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    text,
-    html,
-  };
+  try {
 
-  await transporter.sendMail(mailOptions);
+    const response = await resend.emails.send({
+      from: process.env.EMAIL_FROM,
+      to,
+      subject,
+      text,
+      html,
+    });
+
+    console.log('EMAIL SENT:', response);
+
+    return response;
+
+  } catch (error) {
+
+    console.log('EMAIL ERROR:', error);
+
+    throw new Error('Email sending failed');
+  }
 };
 
 module.exports = { sendEmail };
